09d5ecc8338d4f6bd6fbe60ee3137e93ecdc10f5
ChenTianLe <ctl1992@gmail.com>
2021-09-08 17:23:02 +0200

    添加西班牙语常用100句

--- a1_a2_b1.py
+++ a1_a2_b1.py
@@ -3,21 +3,21 @@ import shutil
 import os
 import pandas
 
-def mergeCsv():
-    if os.path.exists("merge.xls"):
-        os.remove("merge.xls")
+def a1_a2_b1Csv():
+    if os.path.exists("a1_a2_b1.xls"):
+        os.remove("a1_a2_b1.xls")
     a1csv = pandas.read_excel("a1.xls")
     a2csv = pandas.read_excel("a2.xls")
     b1csv = pandas.read_excel("b1.xls")
     a1csv.append(a2csv, ignore_index=True) \
         .append(b1csv, ignore_index=True) \
-        .to_excel("merge.xls", index=False, header=False)
+        .to_excel("a1_a2_b1.xls", index=False, header=False)
     pass
 
-def mergeMp3():
-    if os.path.exists("merge.mp3"):
-        os.remove("merge.mp3")
-    with open('merge.mp3', 'wb') as f:
+def a1_a2_b1Mp3():
+    if os.path.exists("a1_a2_b1.mp3"):
+        os.remove("a1_a2_b1.mp3")
+    with open('a1_a2_b1.mp3', 'wb') as f:
         a1mp3 = open('a1.mp3', 'rb')
         a2mp3 = open('a2.mp3', 'rb')
         b1mp3 = open('b1.mp3', 'rb')
@@ -26,6 +26,6 @@ def mergeMp3():
         shutil.copyfileobj(b1mp3, f)
     pass
 
-mergeCsv()
-mergeMp3()
+a1_a2_b1Csv()
+a1_a2_b1Mp3()
 
